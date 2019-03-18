425ef6afb11460249d9da7b1d788fc3afa4aee16
Miroslaw <bytesbay@icloud.com>
2019-03-18 17:17:53 +0200

    sdadqwe

---
+++ resources/assets/js/admin/views/Index.vue
@@ -0,0 +1,36 @@
+<template>
+  <main>
+    <div class="page-title">ADMIN PANEL</div>
+
+    <table class="table">
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Registration Date</th>
+          <th>Phone</th>
+          <th class="text-center">Email</th>
+          <th class="text-center">Aproved</th>
+          <th class="text-center">Account</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr class="user">
+          <th>John Snow</th>
+          <th>02 / 02 /2019</th>
+          <th>+380557744876</th>
+          <th>Johnsnow14@gmail.com</th>
+          <th class="text-center"><i class="fas fa-check-circle"></i></th>
+          <th class="text-center">Individual</th>
+        </tr>
+      </tbody>
+    </table>
+  </main>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
