fee5ee3f58f428cccbac62dc314fb77175332813
a.shkuropatov <a.shkuropatov.workbox@gmail.com>
2019-03-19 11:43:52 +0200

    BIG sidebar update + deposit page

--- resources/views/headAssets.blade.php
+++ resources/views/headAssets.blade.php
@@ -1,4 +1,4 @@
-<link href="https://fonts.googleapis.com/css?family=Rubik:300,400,500,700,700i,900&amp;subset=cyrillic,hebrew" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Rubik:300,400,400i,500,700,700i,900&amp;subset=cyrillic,hebrew" rel="stylesheet">
 <link href="{{ asset('css/font-awesome/css/fontawesome-all.min.css') }}" rel="stylesheet">
 <link href="{{ asset('css/nice-select.css') }}" rel="stylesheet">
 <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/css/select2.min.css" rel="stylesheet">
--- resources/views/profileFAQ.blade.php
+++ resources/views/profileFAQ.blade.php
@@ -10,67 +10,8 @@
    
     <body>
       
-      <header id="profile_header">
-        <div class="logo-wrap">
-          <img src="/img/logo.png" alt="">
-        </div>
 
-        <div class="header-search-wrapper">
-          <form class="header-search">
-            <input type="text" name="search" placeholder="Search...">
-            <button type="submit">
-              <i class="fas fa-search"></i>
-            </button>
-          </form>
-        </div>
-
-        <div class="profile-avatar profile-header-btn">
-          <div class="img-wrap">
-            <img src="/img/profile-avatar.png" alt="">
-          </div>
-        </div>
-
-        <div class="notifications profile-header-btn">
-          <div class="ico-wrap">
-            <i class="fas fa-bell"></i>
-          </div>
-        </div>
-
-        <div class="dots-wrapper profile-header-btn">
-          <div class="ico-wrap">
-            <i class="fas fa-ellipsis-h"></i>
-          </div>
-        </div>
-      </header>
-
-      <aside class="profile-nav">
-        <ul class="links-container">
-          <li class="link-item">
-            <i class="fas fa-check-circle"></i>
-            Exchange
-          </li>
-          <li class="link-item">
-            <i class="fas fa-exchange"></i>
-            Currancy Rate
-          </li>
-          <li class="link-item">
-            <i class="fas fa-chart-bar"></i>
-            Transition
-          </li>
-          <li class="link-item">
-            <i class="fas fa-envelope"></i>
-            Contact
-          </li>
-          <li class="link-item">
-            <i class="fas fa-question"></i>
-            FAQ
-          </li>
-          <li class="link-item">
-            <i class="fas fa-cog"></i>
-            Settings
-          </li>
-        </ul>
-      </aside>
+      @include('profileParts/sidebar')
 
       <div class="profile-content">
         <div class="page-title"><b>FAQ</b> - Support Panel</div>
